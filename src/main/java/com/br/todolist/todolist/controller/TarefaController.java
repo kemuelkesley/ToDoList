c7206bc5eefad8440082e0a6f0511731e9dcddf3
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2023-06-22 08:57:59 -0300

    Adicionado Template novo, para melhor experiencia com o usuario final, como também um footer novo e um navbar.

--- src/main/java/com/br/todolist/todolist/controller/TarefaController.java
+++ src/main/java/com/br/todolist/todolist/controller/TarefaController.java
@@ -1,5 +1,6 @@
 package com.br.todolist.todolist.controller;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -8,10 +9,7 @@ import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.br.todolist.todolist.model.Tarefa;
 import com.br.todolist.todolist.repository.TarefaRepository;
@@ -72,19 +70,19 @@ public class TarefaController {
 		return "redirect:/lista/tarefas";
 	}
 
-//	@GetMapping("/lista/tarefas")
-//	public String tarefasConcluida(Model model) {
-//		List<Tarefa> listaTarefas = tarefaRepo.findAll();
-//		model.addAttribute("listaTarefas", listaTarefas);
-//		model.addAttribute("tarefa", new Tarefa());
-//
-//		List<Boolean> tarefasConcluidas = listaTarefas.stream()
-//				.map(Tarefa::isCompletado)
-//				.collect(Collectors.toList());
-//		model.addAttribute("tarefasConcluidas", tarefasConcluidas);
-//
-//		return "/lista/tarefas/index";
-//	}
+	//	@GetMapping("/lista/tarefas")
+	//	public String tarefasConcluida(Model model) {
+	//		List<Tarefa> listaTarefas = tarefaRepo.findAll();
+	//		model.addAttribute("listaTarefas", listaTarefas);
+	//		model.addAttribute("tarefa", new Tarefa());
+	//
+	//		List<Boolean> tarefasConcluidas = listaTarefas.stream()
+	//				.map(Tarefa::isCompletado)
+	//				.collect(Collectors.toList());
+	//		model.addAttribute("tarefasConcluidas", tarefasConcluidas);
+	//
+	//		return "/lista/tarefas/index";
+	//	}
 
 
 }
