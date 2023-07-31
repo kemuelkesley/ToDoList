300254f1118d618146b93eb35de3e9f119d750fc
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2023-07-31 14:23:53 -0300

    Alterações novas e no controller.

--- src/main/java/com/br/todolist/todolist/controller/TarefaController.java
+++ src/main/java/com/br/todolist/todolist/controller/TarefaController.java
@@ -1,19 +1,20 @@
 package com.br.todolist.todolist.controller;
 
-import java.io.Serializable;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.br.todolist.todolist.model.Tarefa;
 import com.br.todolist.todolist.repository.TarefaRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class TarefaController {
@@ -39,10 +40,10 @@ public class TarefaController {
 	}
 
 	@PostMapping("/tarefas/salvar")
-	public String salvarTarefa(@ModelAttribute("tarefa") Tarefa tarefa, BindingResult bindingResult, Model model) {
+	public String salvarTarefa(@ModelAttribute("tarefa") @Valid Tarefa tarefa, BindingResult bindingResult, Model model) {
 
 		if (bindingResult.hasErrors()) {
-			return "lista/tarefas";
+			return "/tarefas/form";
 		}
 
 		tarefaRepo.save(tarefa);
--- src/main/java/com/br/todolist/todolist/model/Tarefa.java
+++ src/main/java/com/br/todolist/todolist/model/Tarefa.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 
 @Entity
@@ -16,7 +18,8 @@ public class Tarefa {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-		
+	
+	
 	private String descricao;
 
 	protected boolean completado;
